bed952220f5096dc434209e63409b6e5e257ece5
qs0va <babkovsv@ya.ru>
2025-12-07 21:16:37 +0300

    init with spring initializr

---
+++ mafia-accounter/src/main/java/ru/pet/sb/mafia/MafiaAccounterApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.sb.mafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaAccounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaAccounterApplication.class, args);
+	}
+
+}
---
+++ mafia-accounter/src/test/java/ru/pet/sb/mafia/MafiaAccounterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.sb.mafia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaAccounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
